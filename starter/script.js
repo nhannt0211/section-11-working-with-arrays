56bc55f086d949eb0c74552346d84bbf715a6f9c
Nhan Nguyen <nhannt@2-b.io>
2021-10-28 13:59:07 +0700

    143

--- starter/script.js
+++ starter/script.js
@@ -65,12 +65,141 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+
+
+
+/////////////////////////////////////////////////
+
+/**
+ * 141. Simple Array Methods
+ */
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+//SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//JOIN
+console.log(letters.join("-"));
+*/
+
+
+/**
+ * 142. Looping Arrays: forEach 
+ */
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// console.log("========  Loop array with forEach method ========");
+// movements.forEach(function(mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// })
+
+
+/**
+ * Execise 12/10/2021
+ */
+// function solution(S) {
+//   let tempArr = [];
+//   let rs = 1;
+//   for (let i = 0; i < S.length; i++) {
+//     let currentChar = S[i];
+//     if (tempArr[currentChar] == undefined) {
+//       tempArr[currentChar] = true;
+//     }
+//     else {
+//       rs++;
+//       tempArr = [];
+//       tempArr[currentChar] = true;
+//     }
+//   }
+//   return rs;
+// }
+// console.log('Result: ', solution('abab'));
+// console.log("===========================");
+// console.log('Result: ', solution('world'));
+// console.log("===========================");
+// console.log('Result: ', solution('dddd'));
+// console.log("===========================");
+// console.log('Result: ', solution('cycle'));
+// console.log("===========================");
+// console.log('Result: ', solution('abba'));
+// console.log("===========================");
+// console.log('Result: ', solution('abcadbea'));
+
+
+/**
+ * Exercise 13/10/2021
+ */
+// function solution(N) {
+//   let arr = N.toString().split('');
+//   let rs = -9999999999;
+//   let tmpArr = []
+//   for (let i = 0; i < arr.length; i++) {
+//     tmpArr = [...arr];
+//     if (arr[i] == 5) {
+//       tmpArr.splice(i, 1)
+//       const resultString = tmpArr.join('');
+//       if (rs < parseInt(resultString))
+//         rs = (parseInt(resultString) != -0) ? parseInt(resultString) : 0;
+//     }
+//   }
+//   return rs;
+// }
+// console.log(solution(15958));
+// console.log(solution(-5859));
+// console.log(solution(-5000));
+// console.log(solution(-9595000));
+
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function(value, key, map) {
+  console.log(`${key}: ${value}`);
+})
 
-/////////////////////////////////////////////////
+//Set
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"])
+console.log(currenciesUnique);
+currenciesUnique.forEach(function(value, _, set) {
+  console.log(`${value}: ${value}`);
+})
